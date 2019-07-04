171276cecfe0209410a6217a2c01cc6e75b8ded9
Jyr Gaxiola <jyr.gaxiola@gmail.com>
2019-07-03 23:35:53 -0500

    fix(stations models): Generate model id from pre save signal

--- apps/stations/models.py
+++ apps/stations/models.py
@@ -1,8 +1,13 @@
 # coding: utf8
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.db import models
 
 from apps.utils import create_id
 
+@receiver(pre_save)
+def pre_save(sender, instance, *args, **kwargs):
+    instance.id = create_id(instance.prefix)
 
 class Location(models.Model):
     """ Location object is the representation of physical station
@@ -15,8 +20,7 @@ class Location(models.Model):
             geometry -- Similar to coordinate but using with postgis
     """
 
-    id = models.CharField(default=create_id('loc_'), primary_key=True,
-                          max_length=30, unique=True)
+    id = models.CharField(primary_key=True, max_length=30, unique=True)
     name = models.CharField(max_length=100)
     latitude = models.DecimalField(max_digits=19, decimal_places=16)
     longitude = models.DecimalField(max_digits=19, decimal_places=16)
@@ -24,12 +28,24 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+    @property
+    def prefix(self):
+        prefix = self.__class__.__name__.lower()[0:3] + '_'
+        return prefix
+
 
 class Station(models.Model):
     ## TODO: add docstring
 
-    id = models.CharField(default=create_id('sta_'), primary_key=True,
-                          max_length=30, unique=True)
+    id = models.CharField(primary_key=True, max_length=30, unique=True)
     location = models.ForeignKey(Location, on_delete=models.DO_NOTHING)
     order = models.IntegerField(default=0)
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.id + self.location.name
+
+    @property
+    def prefix(self):
+        prefix = self.__class__.__name__.lower()[0:3] + '_'
+        return prefix
